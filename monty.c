db8dde8802f9d9ae8f85f33adeda65b34ebcac82
Muhammad Mansur <muhammadmansur737@gmail.com>
2023-10-19 07:02:00 -0700

    fx

--- monty.c
+++ monty.c
@@ -1,6 +1,27 @@
 #include "monty.h"
 
 /**
+  * pint - implements the pint opcode
+  *
+  * @line_no: line number
+  *
+  * Return: void
+  */
+
+void pint(int line_no)
+{
+	int up = -1;
+
+	if (up >= 0)
+		printf("%d\n", stack[up]);
+	else
+	{
+		fprintf(stderr, "L%d: Error: can't pint, stack empty\n", line_no);
+		exit(EXIT_FAILURE);
+	}
+}
+
+/**
   * main - entry point
   *
   * @argc: argument count
